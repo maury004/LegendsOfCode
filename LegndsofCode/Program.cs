a3dd7b90c5384a6b6375454c5936b972ae00c869
maury004 <maury004@gmail.com>
2022-02-04 10:19:17 -0500

    Cambio Inicial

---
+++ LegndsofCode/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegndsofCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            Console.WriteLine(EvaluarCadena(SepararCadena(input)));
+        }
+
+        public static String[] SepararCadena(String parametro)
+        {
+            return parametro.Split(' ');
+        }
+
+        public static List<int> EvaluarCadena(String[] cadena)
+        {
+            int contador = Convert.ToInt32(cadena[0]);
+            List<int> listaNumeros = new List<int>();
+            if ((contador*2) != (cadena.Length +1))
+            {
+                throw new Exception();
+            } else
+            {
+                int iterador = 1;
+                int inicio = Convert.ToInt32(cadena[iterador]);
+                int fin = Convert.ToInt32(cadena[iterador+1]);
+
+
+                for (int i = 0; i < contador; i++)
+                {
+
+                    for(int j = inicio; j <= fin; j++)
+                    {
+                        listaNumeros.Add(j);
+                    }
+                    iterador++;
+                    inicio = Convert.ToInt32(cadena[iterador]);
+                    fin = Convert.ToInt32(cadena[iterador + 1]);
+                }
+            }
+            return listaNumeros;
+        }
+    }
+}
